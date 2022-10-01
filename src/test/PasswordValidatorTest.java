3401421f90a78ca8d137c5813820b8da7f8a6998
sg0224460 <AmiyaPrasad.Lenka@sabre.com>
2022-10-01 14:01:35 +0530

    Created custom exception class InvalidPasswordException

--- src/test/PasswordValidatorTest.java
+++ src/test/PasswordValidatorTest.java
@@ -1,21 +1,24 @@
 package test;
 
 public class PasswordValidatorTest {
-    // case#1 : Invalid password as null value
+    // case#1 : Invalid password as null value, expect that it will throw InvalidPasswordException.
 
-    // case#2 : Invalid password with no lowercase character
+    // case#2 : Invalid password with no lowercase character, expect that it will throw InvalidPasswordException.
 
-    // case#3 : Invalid password with length less than 9
+    // case#3 : Invalid password with length less than 9, expect that it will throw InvalidPasswordException.
 
-    // case#4 : inValid password with length less than 9 and no upper case character
+    // case#4 : inValid password with length less than 9 and no upper case character,
+    // expect that it will throw InvalidPasswordException.
 
-    // case#5 : Invalid password with only numbers
+    // case#5 : Invalid password with only numbers, expect that it will throw InvalidPasswordException.
 
-    // case#6 : Valid password with length less than 9
+    // case#6 : Valid password with length less than 9.
 
-    // case#7 : Valid password with no uppercase character but meets 3 conditions - not null, lower case exists, length greater than 8 and also number exists
+    // case#7 : Valid password with no uppercase character but meets 3 conditions - not null,
+    // lower case exists, length greater than 8 and also number exists.
 
-    // case#8 : Valid password with no uppercase character but meets 3 conditions - not null, lower case exists, length greater than 8
+    // case#8 : Valid password with no uppercase character but meets 3 conditions - not null, lower case exists,
+    // length greater than 8.
 
     // case#9 : Valid password with no number
 
