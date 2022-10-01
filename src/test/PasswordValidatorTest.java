8ffd030cc7c549eaee51da0fb1e7d7ec2186405f
sg0224460 <AmiyaPrasad.Lenka@sabre.com>
2022-10-01 13:53:55 +0530

    Identified the test cases to be covered

--- src/test/PasswordValidatorTest.java
+++ src/test/PasswordValidatorTest.java
@@ -1,4 +1,23 @@
 package test;
 
 public class PasswordValidatorTest {
+    // case#1 : Invalid password as null value
+
+    // case#2 : Invalid password with no lowercase character
+
+    // case#3 : Invalid password with length less than 9
+
+    // case#4 : inValid password with length less than 9 and no upper case character
+
+    // case#5 : Invalid password with only numbers
+
+    // case#6 : Valid password with length less than 9
+
+    // case#7 : Valid password with no uppercase character but meets 3 conditions - not null, lower case exists, length greater than 8 and also number exists
+
+    // case#8 : Valid password with no uppercase character but meets 3 conditions - not null, lower case exists, length greater than 8
+
+    // case#9 : Valid password with no number
+
+    // case#10 : Valid password with all meeting criteria
 }
--- src/utility/PasswordValidator.java
+++ src/utility/PasswordValidator.java
@@ -5,4 +5,17 @@ package utility;
  * PasswordValidator utility is responsible to validate the given password again a set-up of business rules.
  */
 public class PasswordValidator {
+
+    public static void main(String arg[]) {
+        String password = "October@01";
+        if (validatePassword(password)) {
+            System.out.println("The given password is valid!");
+        } else {
+            System.out.println("The given password is not valid!! ");
+        }
+    }
+
+    public static boolean validatePassword(String password) {
+        return true;
+    }
 }
