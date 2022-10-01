7471a9b07f5815f34626d79c03f2db9a1fe60b77
sg0224460 <AmiyaPrasad.Lenka@sabre.com>
2022-10-01 13:05:10 +0530

    Created the skeleton of the class and test class

---
+++ src/utility/PasswordValidator.java
@@ -0,0 +1,8 @@
+package utility;
+
+/**
+ * Password verification
+ * PasswordValidator utility is responsible to validate the given password again a set-up of business rules.
+ */
+public class PasswordValidator {
+}
